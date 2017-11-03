04c83c389d0697e7519de06cddad072fa9083ba5
unknown <sonnet22@gmil.com>
2017-11-03 12:41:38 -0400

    fix: added selectProduct() call to conditional statement

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,6 +12,7 @@ db.connect(function(err) {
 function start() {
   db.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
+    console.log("Welcome to Bamazon!");
     for (var i in res) {
       console.log(`${res[i].item_id} || ${res[i].product_name} || $${res[i].price}`);
     }
@@ -20,55 +21,57 @@ function start() {
 }
 
 function selectProduct() {
-      inquirer.prompt([
-      {
-        name: "choice",
-        type: "input",
-        message: "Which product would you like to purchase?"
-      },
-      {
-        name: "quantity",
-        type: "input",
-        message: "How many of this item would you like?"
-      }
-    ])
-    .then(function(answer) {
-      db.query("SELECT * FROM products WHERE item_id = ?", [answer.choice],
-      function(err, res) {
-        if (err) throw err;
-        var choice = parseInt(answer.choice),
-            requestedQuantity = parseInt(answer.quantity),
-            chosenItem;
+  inquirer.prompt([
+  {
+    name: "choice",
+    type: "input",
+    message: "Which product would you like to purchase?"
+  },
+  {
+    name: "quantity",
+    type: "input",
+    message: "How many of this item would you like?"
+  }
+  ])
+  .then(function(answer) {
+    db.query("SELECT * FROM products WHERE item_id = ?", [answer.choice],
+    function(err, res) {
+      if (err) throw err;
+      var choice = parseInt(answer.choice),
+          requestedQuantity = parseInt(answer.quantity),
+          chosenItem;
 
-            for (var i = 0; i < res.length; i++) {
-              if (res[i].item_id === choice) {
-                chosenItem = res[i];
-              }
+          // Check for matching id in database
+          for (var i = 0; i < res.length; i++) {
+            if (res[i].item_id === choice) {
+              chosenItem = res[i];
             }
-            var itemId = chosenItem.item_id,
-            product = chosenItem.product_name,
-            cost = chosenItem.price,
-            currentStock = chosenItem.stock_quantity;
+          }
 
-            if (choice <= itemId) {
-              console.log("Found a match for " + product + "!");
-              var updatedStock = currentStock - requestedQuantity;
-                  totalCost = cost * requestedQuantity,
-                  totalCost = totalCost.toFixed(2);
+          var itemId = parseInt(chosenItem.item_id),
+          product = chosenItem.product_name,
+          cost = chosenItem.price,
+          currentStock = parseInt(chosenItem.stock_quantity);
 
-              db.query("UPDATE products SET ? WHERE ?",
-              [{stock_quantity: updatedStock}, {item_id: choice}],
-              function(err, res) {
-                console.log(`Thank you for your ${requestedQuantity}
-                  order(s) of ${product}. The total amount due is ${totalCost}.`);
-              });
+          if (requestedQuantity <= currentStock) {
+            console.log("Found a match for " + product + "!");
+            var updatedStock = currentStock - requestedQuantity;
+                totalCost = cost * requestedQuantity,
+                totalCost = totalCost.toFixed(2);
 
-            } else {
-              console.log("Insufficient quantity!");
-            }
-      })
+            db.query("UPDATE products SET ? WHERE ?",
+            [{stock_quantity: updatedStock}, {item_id: choice}],
+            function(err, res) {
+              console.log(`Thank you for your ${requestedQuantity}
+                order(s) of ${product}. The total amount due is ${totalCost}.`);
+            });
+          } else {
+            console.log("Insufficient quantity!");
+            selectProduct()
+          }
     })
-    .catch(function(rejection) {
-      console.log("Reason for rejection " + rejection);
-    });
+  })
+  .catch(function(rejection) {
+    console.log("Reason for rejection " + rejection);
+  });
 }
